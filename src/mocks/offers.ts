7afc39bbe05aa6e526e48e439f206c6f5ff54ef5
Daniil888-m <daniil.kolotygin80@gmail.com>
2025-05-23 23:07:31 +0300

    подготовил моки для предложений

---
+++ src/mocks/offers.ts
@@ -0,0 +1,117 @@
+export const offersMock = [
+  {
+    'id': '6af6f711-c28d-4121-82cd-e0b462a27f00',
+    'title': 'Beautiful & luxurious studio at great location',
+    'type': 'apartment',
+    'price': 120,
+    'city': {
+      'name': 'Amsterdam',
+      'location': {
+        'latitude': 52.35514938496378,
+        'longitude': 4.673877537499948,
+        'zoom': 8
+      }
+    },
+    'location': {
+      'latitude': 52.35514938496378,
+      'longitude': 4.673877537499948,
+      'zoom': 8
+    },
+    'isFavorite': false,
+    'isPremium': false,
+    'rating': 4,
+    'previewImage': 'https://url-to-image/image.png'
+  },
+  {
+    'id': 'gr465711-c28d-4121-82cd-e0b756a27f00',
+    'title': 'Canal View Prinsengracht',
+    'type': 'apartment',
+    'price': 130,
+    'city': {
+      'name': 'Amsterdam',
+      'location': {
+        'latitude': 52.35514938496378,
+        'longitude': 4.673877537499948,
+        'zoom': 8
+      }
+    },
+    'location': {
+      'latitude': 54.35514938496378,
+      'longitude': 4.673877537499948,
+      'zoom': 8
+    },
+    'isFavorite': false,
+    'isPremium': false,
+    'rating': 4,
+    'previewImage': 'https://url-to-image/image.png'
+  },
+  {
+    'id': 'jkk8a711-c28d-4121-82cd-e0b756a27f00',
+    'title': 'Canal View Prinsengracht',
+    'type': 'apartment',
+    'price': 100,
+    'city': {
+      'name': 'Amsterdam',
+      'location': {
+        'latitude': 52.35514938496378,
+        'longitude': 4.673877537499948,
+        'zoom': 8
+      }
+    },
+    'location': {
+      'latitude': 54.35514938496378,
+      'longitude': 4.673877537499948,
+      'zoom': 8
+    },
+    'isFavorite': false,
+    'isPremium': false,
+    'rating': 5,
+    'previewImage': 'https://url-to-image/image.png'
+  },
+  {
+    'id': '7908a711-c28d-4121-82cd-e0b756a27f00',
+    'title': 'Canal View Prinsengracht',
+    'type': 'apartment',
+    'price': 90,
+    'city': {
+      'name': 'Amsterdam',
+      'location': {
+        'latitude': 52.35514938496378,
+        'longitude': 4.673877537499948,
+        'zoom': 8
+      }
+    },
+    'location': {
+      'latitude': 54.35514938496378,
+      'longitude': 4.673877537499948,
+      'zoom': 8
+    },
+    'isFavorite': false,
+    'isPremium': false,
+    'rating': 3,
+    'previewImage': 'https://url-to-image/image.png'
+  },
+  {
+    'id': 'vfj9a711-c28d-4121-82cd-e0b756a27f00',
+    'title': 'Beautiful & luxurious studio at great location',
+    'type': 'apartment',
+    'price': 120,
+    'city': {
+      'name': 'Paris',
+      'location': {
+        'latitude': 52.35514938496378,
+        'longitude': 4.673877537499948,
+        'zoom': 8
+      }
+    },
+    'location': {
+      'latitude': 54.35514938496378,
+      'longitude': 4.673877537499948,
+      'zoom': 8
+    },
+    'isFavorite': false,
+    'isPremium': false,
+    'rating': 5,
+    'previewImage': 'https://url-to-image/image.png'
+  }
+];
