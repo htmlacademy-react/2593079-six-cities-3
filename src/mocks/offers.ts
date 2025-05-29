b7236895a807da1279b32c82f531cf104ba0fcd8
Daniil888-m <daniil.kolotygin80@gmail.com>
2025-05-29 22:50:32 +0300

    изменения моков

--- src/mocks/offers.ts
+++ src/mocks/offers.ts
@@ -38,8 +38,8 @@ export const offersMock = [
       }
     },
     'location': {
-      'latitude': 52.35514938496378,
-      'longitude': 4.673877537499948,
+      'latitude': 52.3909553943508,
+      'longitude': 4.85309666406198,
       'zoom': 8
     },
     'isFavorite': false,
@@ -61,8 +61,8 @@ export const offersMock = [
       }
     },
     'location': {
-      'latitude': 54.35514938496378,
-      'longitude': 4.673877537499948,
+      'latitude': 52.3609553943508,
+      'longitude': 4.85309666406198,
       'zoom': 8
     },
     'isFavorite': false,
@@ -84,8 +84,8 @@ export const offersMock = [
       }
     },
     'location': {
-      'latitude': 54.35514938496378,
-      'longitude': 4.673877537499948,
+      'latitude': 52.3909553943508,
+      'longitude': 4.929309666406198,
       'zoom': 8
     },
     'isFavorite': true,
@@ -107,8 +107,8 @@ export const offersMock = [
       }
     },
     'location': {
-      'latitude': 54.35514938496378,
-      'longitude': 4.673877537499948,
+      'latitude': 52.3809553943508,
+      'longitude': 4.939309666406198,
       'zoom': 8
     },
     'isFavorite': true,
