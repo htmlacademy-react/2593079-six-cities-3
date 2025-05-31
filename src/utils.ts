b0e610455142e8ec4d60da5fcb2bd0e2fbdcc566
Daniil888-m <daniil.kolotygin80@gmail.com>
2025-05-31 21:55:30 +0300

    установил dayjs и подготовил функции для обработки даты

--- src/utils.ts
+++ src/utils.ts
@@ -1,5 +1,9 @@
+import dayjs from 'dayjs';
+
 import { Offer } from './mocks/offers';
 
 const filterByCity = (offers: Offer[], cityName: string) => offers.filter((currOffer) => currOffer.city.name === cityName);
+const parseReviewDate = (date: string) => dayjs(date).format('MMMM YYYY');
+const parseDateTime = (date: string) => dayjs(date).format('YYYY-MM-DD');
 
-export {filterByCity};
+export {filterByCity, parseReviewDate, parseDateTime};
