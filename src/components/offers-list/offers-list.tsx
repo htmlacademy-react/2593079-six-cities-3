fc760a1e4e4ad6620b601487c0e4750898f0ffb1
Daniil888-m <daniil.kolotygin80@gmail.com>
2025-06-02 20:56:33 +0300

    подготовлены компоненты для карточки ближайших предложений и компонент-список для вывода

--- src/components/offers-list/offers-list.tsx
+++ src/components/offers-list/offers-list.tsx
@@ -1,18 +1,32 @@
 import { Offer } from '../../mocks/offers';
+import NearOfferCard from '../near-offer-card/near-offer-card';
 import PlaceCard from '../place-card/place-card';
 
 type OffersListProps = {
   offers: Offer[];
   isForFavPage?: boolean;
+  isForOfferPage?: boolean;
   onChange?: (activeOfferId: string) => void;
 }
 
-export default function OffersList({offers, isForFavPage, onChange}: OffersListProps): JSX.Element {
+const getListClassName = (isForFavPage: boolean | undefined, isForOfferPage: boolean | undefined) => {
+  if(isForFavPage) {
+    return 'favorites__places';
+  } else if(isForOfferPage) {
+    return 'near-places__list places__list';
+  }
+  return 'cities__places-list tabs__content places__list';
+};
+
+export default function OffersList({offers, isForFavPage, isForOfferPage, onChange}: OffersListProps): JSX.Element {
 
   return (
-    <div className={isForFavPage ? 'favorites__places' : 'cities__places-list places__list tabs__content'}>
+    <div className={getListClassName(isForFavPage, isForOfferPage)}>
       {offers.map((currOffer) => {
         const key = currOffer.id;
+        if(isForOfferPage) {
+          return <NearOfferCard offer={currOffer} key={key}/>;
+        }
         return <PlaceCard key={key} offer={currOffer} isForFavPage={isForFavPage} onChange={onChange}/>;
 
       })}
