dbcb0071b22dacb6a62ee857ecda812a5a850a7e
Daniil888-m <daniil.kolotygin80@gmail.com>
2025-06-08 22:55:36 +0300

    создан компонент, отвечающий за выбор сортировки, а также функционал, который выполняет сортировку

--- src/components/main-page-screen/main-page-screen.tsx
+++ src/components/main-page-screen/main-page-screen.tsx
@@ -1,11 +1,12 @@
 import { useState } from 'react';
 import OffersList from '../offers-list/offers-list';
-import { Cities } from '../../const';
+import { Cities, OptionsTypes, SortFunctions } from '../../const';
 import { filterByCity } from '../../utils';
 import Map from '../map/map';
 import { useAppSelector } from '../../hooks/store';
 import CitiesList from '../cities-list/cities-list';
 import { Offer } from '../../mocks/offers';
+import OptionsList from '../options-list/options-list';
 
 type MainPageScreenProps = {
   offers: Offer[] | undefined;
@@ -13,11 +14,15 @@ type MainPageScreenProps = {
 
 export default function MainPageScreen({offers}: MainPageScreenProps): JSX.Element {
   const activeCity = useAppSelector((state) => state.activeCity);
+  const [currentOption, setCurrentOption] = useState<OptionsTypes>(OptionsTypes.POP);
   const [activeOffer, setActiveOffer] = useState<string | null>(null);
 
   const handleActiveOfferChange = (activeOfferId: string) => setActiveOffer(activeOfferId);
 
-  const filteredOffers = filterByCity(offers, activeCity);
+  let filteredOffers = filterByCity(offers, activeCity);
+  if(currentOption !== OptionsTypes.POP) {
+    filteredOffers = SortFunctions[currentOption](filteredOffers);
+  }
   const activeCityData = filteredOffers?.length ? filteredOffers[0].city : null;
 
   return (
@@ -31,21 +36,7 @@ export default function MainPageScreen({offers}: MainPageScreenProps): JSX.Eleme
           <section className="cities__places places">
             <h2 className="visually-hidden">Places</h2>
             <b className="places__found">{filteredOffers?.length} places to stay in {activeCity}</b>
-            <form className="places__sorting" action="#" method="get">
-              <span className="places__sorting-caption">Sort by   </span>
-              <span className="places__sorting-type" tabIndex={0}>
-                   Popular
-                <svg className="places__sorting-arrow" width="7" height="4">
-                  <use xlinkHref="#icon-arrow-select"/>
-                </svg>
-              </span>
-              <ul className="places__options places__options--custom">
-                <li className="places__option places__option--active" tabIndex={0}>Popular</li>
-                <li className="places__option" tabIndex={0}>Price: low to high</li>
-                <li className="places__option" tabIndex={0}>Price: high to low</li>
-                <li className="places__option" tabIndex={0}>Top rated first</li>
-              </ul>
-            </form>
+            <OptionsList currentOption={currentOption} changeOption={setCurrentOption}/>
             <OffersList offers={filteredOffers} onChange={handleActiveOfferChange}/>
           </section>
           <div className="cities__right-section">
