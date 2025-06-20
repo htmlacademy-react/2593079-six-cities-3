689d6bca00192a49b2d5eab8f4ab9aa2f19085f7
Daniil888-m <daniil.kolotygin80@gmail.com>
2025-06-20 23:07:33 +0300

    настроили дополнительную middleware для обработки запроса на Redirect

--- src/store/index.ts
+++ src/store/index.ts
@@ -2,6 +2,8 @@ import { configureStore } from '@reduxjs/toolkit';
 import { reducer } from './reducer.ts';
 import createAPI from '../services/api.ts';
 import { getToken } from '../services/token.ts';
+import { redirect } from './middlewares/redirect.ts';
+
 
 const api = createAPI();
 api.interceptors.request.use(
@@ -19,6 +21,6 @@ export const store = configureStore({
     thunk: {
       extraArgument: api,
     }
-  })
+  }).concat(redirect)
 });
 
