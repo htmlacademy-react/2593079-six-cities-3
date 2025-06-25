4bdce6a6b4562c4a1ff836d6dc499dd3b973f53f
Daniil888-m <daniil.kolotygin80@gmail.com>
2025-06-25 23:18:52 +0300

    исправлена ошибка импорта

--- src/hooks/store.ts
+++ src/hooks/store.ts
@@ -1,6 +1,7 @@
 
 import { TypedUseSelectorHook, useDispatch, useSelector } from 'react-redux';
-import { AppDispatch, State } from '../store/types';
+import { AppDispatch } from '../store/types';
+import { State } from '../types/store';
 
 export const useAppDispatch = () => useDispatch<AppDispatch>();
 export const useAppSelector: TypedUseSelectorHook<State> = useSelector;
