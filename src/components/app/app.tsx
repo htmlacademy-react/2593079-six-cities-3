7b4d89ddfca88b599c303aa60da0eaea39d490b3
Daniil888-m <daniil.kolotygin80@gmail.com>
2025-07-06 17:01:48 +0300

    git config для прохождения Github Actions

---
+++ src/components/app/app.tsx
@@ -0,0 +1,38 @@
+import { Route, Routes } from 'react-router-dom';
+import MainPage from '../../pages/main-page/main-page';
+import { RoutePath } from '../../const';
+import Layout from '../layout/layout';
+import PrivateRoute from '../private-route/private-route';
+import LoginPage from '../../pages/login-page/login-page';
+import NotFoundPage from '../../pages/not-found-page/not-found-page';
+import FavoritesPage from '../../pages/favorites-page/favorites-page';
+import OfferPage from '../../pages/offer-page/offer-page';
+
+export default function App(): JSX.Element {
+  return (
+
+    <Routes>
+      <Route path={RoutePath.Main} element={<Layout/>}>
+        <Route index element={<MainPage/>}/>
+        <Route path={RoutePath.Offer} element={<OfferPage/>}/>
+        <Route path={RoutePath.Favorites} element={
+          <PrivateRoute>
+            <FavoritesPage/>
+          </PrivateRoute>
+        }
+        >
+        </Route>
+
+      </Route>
+      <Route path={RoutePath.Login} element={
+        <PrivateRoute onlyUnAuth>
+          <LoginPage/>
+        </PrivateRoute>
+      }
+      />
+      <Route path="*" element={<NotFoundPage/>}/>
+
+    </Routes>
+
+  );
+}
