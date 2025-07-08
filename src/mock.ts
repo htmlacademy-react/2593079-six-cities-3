20a39aa3ea1c6801287f55f5d4f840a0811c1fc6
Daniil888-m <daniil.kolotygin80@gmail.com>
2025-07-08 13:15:57 +0300

    Удалены неиспользуемые переменные

--- src/mock.ts
+++ src/mock.ts
@@ -1,4 +1,4 @@
-import { Offer, OfferData, Review } from './types';
+import { Offer, OfferData } from './types';
 
 export function getMockOfferData(): OfferData {
   return ({
@@ -36,20 +36,6 @@ export function getMockOfferData(): OfferData {
       'https://url-to-image/image.png'
     ],
     'maxAdults': 4
-  });
-}
-
-export function getMockComment(): Review {
-  return ({
-    'id': 'b67ddfd5-b953-4a30-8c8d-bd083cd6b62a',
-    'date': '2019-05-08T14:13:56.569Z',
-    'user': {
-      'name': 'Oliver Conner',
-      'avatarUrl': 'https://url-to-image/image.png',
-      'isPro': false
-    },
-    'comment': 'A quiet cozy and picturesque that hides behind a a river by the unique lightness of Amsterdam.',
-    'rating': 4
   });
 }
 
